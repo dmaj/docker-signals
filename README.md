41780c9ddad86bfca025bde91f33878c4903e6c2
dma <dma@web.de>
2019-10-07 21:02:06 +0200

    colors to better differentiate the output

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ The program "showsig" registers for all available signals. An incoming signal is
 It's a good idea to compile this program as a static binary:
 gcc -o showsig showsig.c -static -I.
 
+showsig has learned colors: red, green, blue, yellow, magenta, cyan
--- showsig.c
+++ showsig.c
@@ -4,33 +4,58 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <unistd.h>
+ 
 
+// http://web.theurbanpenguin.com/adding-color-to-your-output-from-c/
 
+char* color = "\033[0m";
+char* nocolor = "\33[0m";
+pid_t pid;
+ 
 void sig_handler(int signo)
 {
     char * str = strsignal(signo);
-    printf("%2d -> SIG%s\n", signo, str);
-}
+    printf("%sPID: %i   %2d -> SIG%s%s\n", color, pid, signo, str, nocolor);
+};
+ 
 
-int main(void)
+char *setColor (char* color_str)
 {
+    int i = 0;
+    while(color_str[i]) {
+       color_str[i] = tolower(color_str[i]);
+       i++;
+    }
+    if (strcmp(color_str, "red") == 0) return ("\33[0;31m");
+    if (strcmp(color_str, "green") == 0) return ("\33[0;32m");
+    if (strcmp(color_str, "yellow") == 0) return ("\33[0;33m");
+    if (strcmp(color_str, "blue") == 0) return ("\33[0;34m");
+    if (strcmp(color_str, "magenta") == 0) return ("\33[0;35m");
+    if (strcmp(color_str, "cyan") == 0) return ("\33[0;36m");
+    printf ("Available Colors for Output: red, green yello, blue, magenta, cyan\n\n");
+    return ("\033[0m");
+};
+
+int main(int argc, char *argv[])
+{
+    if (argc > 1)
+    {
+        color = setColor (argv[1]);
+    }
     int sig;
-    printf ("\n");
+    pid = getpid();
     for (sig = 1; sig < NSIG; sig++)
     {
         if (signal(sig, sig_handler) == SIG_ERR)
-            printf("Can't catch %i -> %s\n", sig, strsignal(sig));
+            printf("%sPID: %i   Can't catch %i -> %s%s\n", color, pid, sig, strsignal(sig), nocolor);
     }
 
-    pid_t  pid;
-    pid = getpid();
-   
-    printf ("\nGet a list of available signals with \"kill -l\"\n"); 
-    printf ("Send signals to PID = %d\n", pid) ;
-    printf ("Starting listener\n\n");
+    //printf ("%sPID: %i   Get a list of available signals with \"kill -l\"%s\n", color, pid, nocolor);
+    printf ("%sPID: %i   Send signals to PID = %d%s\n", color, pid, pid, nocolor) ;
+    printf ("%sPID: %i   Starting listener%s\n", color, pid, nocolor);
 
-    while(1) 
+    while(1)
         sleep(1);
     return 0;
-}
 
+}
